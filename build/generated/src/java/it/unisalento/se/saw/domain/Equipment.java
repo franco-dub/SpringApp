674089f98acc4f87c21e912d0464615d1a48b1b1
franco-dub <frankie_noob@hotmail.it>
2018-08-20 13:49:08 +0200

    refactor calendar

--- build/generated/src/java/it/unisalento/se/saw/domain/Equipment.java
+++ build/generated/src/java/it/unisalento/se/saw/domain/Equipment.java
@@ -1,5 +1,5 @@
 package it.unisalento.se.saw.domain;
-// Generated Aug 18, 2018, 11:38:32 AM by Hibernate Tools 5.2.0.Final
+// Generated Aug 20, 2018, 1:46:40 PM by Hibernate Tools 5.2.0.Final
 
 
 import java.util.HashSet;
